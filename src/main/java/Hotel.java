555769359b1a8ab41158a49feaf10f508c704e4f
Pelle Vedsmand <pellehald@hotmail.com>
2023-10-26 21:59:58 +0200

    bugfix

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.codehaus.jackson.annotate.JsonIgnore;
-
 import java.util.ArrayList;
 import java.util.List;
 
